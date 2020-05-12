90466325aad5caee84d17c2195edc6e1bc4724cd
imran <mrimranoor@gmail.com>
2020-05-12 19:02:52 +0100

    dynamic caching allows caching of resources as you browse the site in a separate cache store to the main shell cache store

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = "site-static-v1";
+const dynamicCache = "site-dynamic-v1";
 const assets = [
   "/",
   "/index.html",
@@ -43,7 +44,15 @@ self.addEventListener("fetch", (evt) => {
   // console.log("Fetch event:", evt);
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
