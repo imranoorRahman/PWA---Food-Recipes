599706bc3cee9074cf5cc120401b854211c43680
imran <mrimranoor@gmail.com>
2020-05-12 23:01:58 +0100

    updating UI with fetched data from DB

--- js/db.js
+++ js/db.js
@@ -4,14 +4,13 @@ db.collection("recipes").onSnapshot((snapshot) => {
   snapshot.docChanges().forEach((change) => {
     // console.log(change, change.doc.data(), change.doc.id);
 
-    change.type === "added"
-      ? // add the document data to the web page
-        console.log(change)
-      : null;
-
-    change.type === "removed"
-      ? // remove the document data to the web page
-        console.log(change)
-      : null;
+    if (change.type === "added") {
+      // add the document data to the web page
+      renderRecipe(change.doc.data(), change.doc.id);
+    }
+    if (change.type === "removed") {
+      // remove the document data to the web page
+      console.log(change);
+    }
   });
 });
